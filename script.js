af1d8d28e67c74ee3214353dc2828738e0848eaf
Tony Wu <bobacrypto2@gmail.com>
2022-10-11 06:11:04 -0700

    Create player1 and player2 to be playable | Allow both players to take turns | Add function to check for win conditions after round 5

--- script.js
+++ script.js
@@ -3,28 +3,9 @@
 // If you need multiples of something (players!), create them with factories.
 
 // Module to encapsulate just the gameboard code itself
+let round = 0;
+
 const gboard = document.getElementById('gameboard');
-
-const gameboard = (() => {
-    const board = ['', '', '', '', '', '', '', '', ''];
-
-    function createBoard() {
-        for (i = 0; i < board.length; i++) {
-            const boardSquare = document.createElement('div');
-            boardSquare.classList.add('square', `${i}`);
-            boardSquare.addEventListener('click', setMark);
-            gboard.appendChild(boardSquare);
-        }
-    }
-
-    function setMark() {
-        this.innerHTML = Tony.mark;
-    }
-
-    return {
-        createBoard,
-    };
-})();
 
 // Factory Function for creating players
 const Player = (name, mark) => {
@@ -37,4 +18,87 @@ const Player = (name, mark) => {
     }
 }
 
-const Tony = Player('Tony', 'X');
+const player1 = Player('Tony', 'X');
+const player2 = Player('Katie', 'O');
+
+const gameboard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+    const numberClass = ['one', 'two' ,'three', 'one', 'two' ,'three', 'one', 'two' ,'three'];
+    
+    function createBoard() {
+        for (i = 0; i < board.length; i++) {
+            const boardSquare = document.createElement('div');
+            boardSquare.classList.add('square');
+            boardSquare.addEventListener('click', setMark);
+            gboard.appendChild(boardSquare);
+        }
+    }
+
+    function setMark() {
+        if(this.innerHTML == '' && round % 2 != 0) {
+            this.innerHTML = player1.mark;
+            round++;
+        } else if(this.innerHTML == '' && round % 2 == 0) {
+            this.innerHTML = player2.mark;
+            round++;
+        } else {
+            return;
+        }
+
+        checkWinner.condition();
+    }
+    
+    createBoard()
+    const squares = document.querySelectorAll('.square');
+    
+    return {
+        squares,
+    };
+})();
+
+const checkWinner = (() => {
+
+    function condition() {
+        let square = gameboard.squares;
+    
+        // Horizontal 3-in-a-row
+        let aaa = [square[0], square[1], square[2]];
+        let bbb = [square[3], square[4], square[5]];
+        let ccc = [square[6], square[7], square[8]];
+    
+        // Vertical 3-in-a-row
+        let abc1 = [square[0], square[3], square[6]];
+        let abc2 = [square[1], square[4], square[7]];
+        let abc3 = [square[2], square[5], square[8]];
+    
+        // Diaganol conditions
+        let diagTopLeft = [square[0], square[4], square[8]];
+        let diagBottomLeft = [square[6], square[4], square[2]];
+
+        let allConditions = [aaa, bbb, ccc, abc1, abc2, abc3, diagTopLeft, diagBottomLeft];
+    
+        // Starting from the round players are able to create their first possible 3-in-a-row
+        // We will start to check for win conditions
+        if(round => 5) {
+            for(i = 0; i < allConditions.length; i++) {
+                if (allConditions[i].every(checkPlayerWin)) {
+                    console.log('it works');
+                }
+            }
+        }
+
+    }
+
+    function checkPlayerWin(check) {
+        if(round % 2 == 0) {
+            return check.innerHTML == 'X';
+        } else {
+            return check.innerHTML == 'O';
+        }
+    }
+
+    return {
+        condition,
+    }
+
+})();
